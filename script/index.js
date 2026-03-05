8ceece456407b3d69720973440250481d2b968ae
saikat281 <sarkarsaikat3467@gmail.com>
2026-03-05 17:43:00 +0600

    Make modal for  lesson words info  and insert information by calling API

--- script/index.js
+++ script/index.js
@@ -5,11 +5,86 @@ const loadLession = () => {
         .then((json => displayLessons(json.data)));
 }
 
+const RemoveAllActive = () => {
+    const lessonButtons = document.querySelectorAll(".lesson-Button");
+    lessonButtons.forEach(btn => {
+        btn.classList.remove("active");
+    })
+}
+
 const loadLevelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
         .then((res) => res.json())
-        .then((data) => displayLevelWords(data.data));
+        .then((data) => {
+            //Button active
+            RemoveAllActive();
+            const lessonBtn = document.getElementById(`lesson-btn-${id}`)
+            lessonBtn.classList.add("active")
+            //dsiplay
+            displayLevelWords(data.data);
+        })
+
+}
+
+const loadWordDetail = async (id) => {
+
+    const url = `https://openapi.programming-hero.com/api/word/${id}`;
+
+    //console.log(url)
+    const res = await fetch(url);
+    const details = await res.json();
+    displayDetails(details.data);
+
+}
+
+// "word": "Eager",
+// "meaning": "আগ্রহী",
+// "pronunciation": "ইগার",
+// "level": 1,
+// "sentence": "The kids were eager to open their gifts.",
+// "points": 1,
+// "partsOfSpeech": "adjective",
+// "synonyms": [
+// "enthusiastic",
+// "excited",
+// "keen"
+// {/* <p class="bg-blue-100 p-2 rounded-sm">Enthusiastic</p>
+//                 <p class="bg-blue-100 p-2 rounded-sm">excited</p>
+//                 <p class="bg-blue-100 p-2 rounded-sm">keen</p> */}
+
+const createSynonym = (synonym) => {
+    const htmlElements = synonym.map((syn) => `<p class="bg-blue-100 p-2 rounded-sm">${syn}</p>`);
+
+    return htmlElements.join(" ");
+}
+
+
+
+const displayDetails = (details) => {
+
+    console.log(details);
+    const detailsContainer = document.getElementById("details-container")
+    detailsContainer.innerHTML = `
+    <div class="space-y-4">
+        <h1 class="text-2xl font-semibold">${details.word} (<i class="fa-solid fa-microphone-lines"></i>:${details.pronunciation})</h1>
+        <div class="space-y-1">
+            <h3 class="font-semibold">Meaning</h3>
+            <h4 class="font-bangla font-semibold">${details.meaning}</h4>
+        </div>
+        <div class="space-y-1">
+            <h3 class="font-semibold">Example</h3>
+            <h4 class="font-bangla font-semibold">${details.sentence}</h4>
+        </div>
+        <div class="space-y-1">
+            <h3 class="font-bangla font-semibold">সমার্থক শব্দ গুলো</h3>
+            <div class="flex justify-start gap-2">
+                ${createSynonym(details.synonyms)}
+            </div>
+        </div>
+    </div>`;
+    document.getElementById("word_modal").showModal();
+
 }
 
 const displayLevelWords = (words) => {
@@ -17,7 +92,7 @@ const displayLevelWords = (words) => {
     const wordContainer = document.getElementById("word-container");
     wordContainer.innerHTML = '';
 
-    if(words.length == 0){
+    if (words.length == 0) {
         wordContainer.innerHTML = `
         <div class=" col-span-full">
             <img class="mx-auto" src="assets/alert-error.png" alt="">
@@ -38,7 +113,7 @@ const displayLevelWords = (words) => {
             <h1 class="font-bangla text-3xl text-gray-900 font-bold">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি"}"</h1>
 
             <div class="flex justify-between items-center">
-                <button class="bg-[#1A91FF10] hover:bg-[#1A91FF80]   px-3 py-2 rounded-sm cursor-pointer"><i class="fa-solid fa-circle-info"></i></button>
+                <button onclick="loadWordDetail(${word.id})" class="bg-[#1A91FF10] hover:bg-[#1A91FF80]   px-3 py-2 rounded-sm cursor-pointer"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="bg-[#1A91FF10] hover:bg-[#1A91FF80]  px-3 py-2 rounded-sm cursor-pointer"> <i class="fa-solid fa-volume-high"></i> </button>
             </div>
         </div>`
@@ -59,7 +134,7 @@ const displayLessons = (lessons) => {
 
 
         btnDiv.innerHTML = `
-        <button onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary" href="">
+        <button id="lesson-btn-${lesson.level_no}" onclick = "loadLevelWord(${lesson.level_no})" class="lesson-Button btn btn-outline btn-primary" href="">
                 <i class="fa-solid fa-book-open"></i>
                  <span>Lession-${lesson.level_no}</span>
         </button>
